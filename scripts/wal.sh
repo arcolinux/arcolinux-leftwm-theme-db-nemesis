d2fb34b2db4ee82aff82d0805deff424294c21b2
arcolinuxz <arcolinuxinfo@gmail.com>
2022-10-03 08:17:06 +0200

    update

--- scripts/wal.sh
+++ scripts/wal.sh
@@ -6,20 +6,20 @@
 if [ -x "$(command -v wal)" ]; then
   if [ ! -d $HOME/.config/wal/templates ] ; then
     mkdir $HOME/.config/wal/templates &&
-    cp $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.toml  $HOME/.config/wal/templates
+    cp $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.ron  $HOME/.config/wal/templates
   elif
-    [ ! -f $HOME/.config/wal/templates/color.leftwm-theme.toml ] ; then
-        cp -f $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.toml  $HOME/.config/wal/templates
+    [ ! -f $HOME/.config/wal/templates/color.leftwm-theme.ron ] ; then
+        cp -f $HOME/.config/leftwm/themes/current/template-wal/color.leftwm-theme.ron  $HOME/.config/wal/templates
   fi
     my_array=($HOME/.config/leftwm/themes/current/backgrounds/*)
         ####my_array=(/usr/share/backgrounds/*)
 
      wal -i ${my_array[$(( $RANDOM % ${#my_array[@]}))]} -a 80
-    cat ~/.cache/wal/color.leftwm-theme.toml &&
-    ####bat ~/.cache/wal/color.leftwm-theme.toml &&               
-    cp -f ~/.cache/wal/color.leftwm-theme.toml $HOME/.config/leftwm/themes/current/theme.toml &&
+    cat ~/.cache/wal/color.leftwm-theme.ron &&
+    ####bat ~/.cache/wal/color.leftwm-theme.ron &&               
+    cp -f ~/.cache/wal/color.leftwm-theme.ron $HOME/.config/leftwm/themes/current/theme.ron &&
     
-    leftwm command "LoadTheme $HOME/.config/leftwm/themes/current/theme.toml"
+    leftwm command "LoadTheme $HOME/.config/leftwm/themes/current/theme.ron"
 
 else
 
